e54fbf5f71dc80524ba6aef837f6266999a41b9a
Stephen Gilliland <stephenmatsuta@gmail.com>
2014-07-01 21:55:05 -0400

    Added Exponent Function to stevemath

--- stevemath.cpp
+++ stevemath.cpp
@@ -1,17 +1,29 @@
 /*
 
 int abs(int);
+int pow(int, int);
 
 abs(-42);  // => 42
+pow(5, 3); // => 125
 
 */
 #include <iostream>
 using namespace std;
 
 int abs(int);
-
+int pow(int base, int exp);
 int main(){
     return 0;
+}
+int pow(int base, int exp){
+    int num = base;
+    if (exp == 0){
+        return 1;
+    }
+    for (int wee = 1; wee < exp; wee++){
+        num = num*base;
+    }
+    return num;
 }
 int abs(int num){
     if (num > -1){
@@ -19,3 +31,5 @@ int abs(int num){
     }
     return -num;
 }
+
+
