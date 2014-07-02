8b536864007ac92cdc85dc112210b22a6633e2c7
Stephen Gilliland <stephenmatsuta@gmail.com>
2014-07-01 21:18:55 -0400

    Absolute value function!

---
+++ stevemath.cpp
@@ -0,0 +1,23 @@
+/*
+
+int abs(int);
+
+abs(-42);  // => 42
+
+*/
+#include <iostream>
+using namespace std;
+
+int abs(int);
+
+int main(){
+    return 0;
+}
+int abs(int num){
+    if (num > -1){
+        return num;
+    }
+    else{
+        return num*-1;
+    }
+}
